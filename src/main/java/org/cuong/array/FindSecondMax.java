83e4cdba140f0ce8cecca976ec58996e79977257
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-02-23 16:16:41 +0700

    find second max value in array

---
+++ src/main/java/org/cuong/array/FindSecondMax.java
@@ -0,0 +1,29 @@
+package org.cuong.array;
+
+public class FindSecondMax {
+    public static void main(String[] args) {
+        // int[] numbers = { 33, 10, 33, 1, 32, 0, 32 };
+        int[] numbers = { 33, 33 };
+        System.out.println(findSecondMax(numbers));
+        String a = "demo";
+
+    }
+
+    static int findSecondMax(int[] a) {
+        if (a == null || a.length < 2) {
+            throw new IllegalArgumentException("Invali input (The lenght of the array is less than 2)");
+        }
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                secondMax = max;
+                max = a[i];
+            } else if (a[i] > secondMax && a[i] != max) {
+                secondMax = a[i];
+            }
+        }
+        return secondMax;
+
+    }
+}
