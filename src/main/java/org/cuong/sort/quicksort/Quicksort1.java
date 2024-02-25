97aba1015c661bccfca24db98697418198a409b3
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-02-25 17:48:47 +0700

    fix quick sort

--- src/main/java/org/cuong/sort/quicksort/Quicksort1.java
+++ src/main/java/org/cuong/sort/quicksort/Quicksort1.java
@@ -2,23 +2,28 @@ package org.cuong.sort.quicksort;
 
 import java.util.Random;
 
-import org.cuong.array.intro.ArrayUtils;
+public class Quicksort1 {
 
-public class QuickSortYoutubeVersion {
     public static void main(String[] args) {
         Random rand = new Random();
         int[] numbers = new int[10];
+
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = rand.nextInt(100);
         }
 
+        // int[] a = { 1, 8, 7, 3, 4 };
+        // printArray(a);
+        // quicksort(a);
+        // printArray(a);
+
         System.out.println("Before:");
-        ArrayUtils.printArray(numbers);
+        printArray(numbers);
 
         quicksort(numbers);
 
-        System.out.println("After:");
-        ArrayUtils.printArray(numbers);
+        System.out.println("\nAfter:");
+        printArray(numbers);
     }
 
     private static void quicksort(int[] array) {
@@ -26,16 +31,20 @@ public class QuickSortYoutubeVersion {
     }
 
     private static void quicksort(int[] array, int lowIndex, int highIndex) {
-        if (lowIndex >= highIndex)
+
+        if (lowIndex >= highIndex) {
             return;
+        }
 
         int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
         int pivot = array[pivotIndex];
         swap(array, pivotIndex, highIndex);
 
         int leftPointer = partition(array, lowIndex, highIndex, pivot);
+
         quicksort(array, lowIndex, leftPointer - 1);
         quicksort(array, leftPointer + 1, highIndex);
+
     }
 
     private static int partition(int[] array, int lowIndex, int highIndex, int pivot) {
@@ -43,19 +52,31 @@ public class QuickSortYoutubeVersion {
         int rightPointer = highIndex - 1;
 
         while (leftPointer < rightPointer) {
+
+            // Walk from the left until we find a number greater than the pivot, or hit the
+            // right pointer.
             while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
                 leftPointer++;
             }
+
+            // Walk from the right until we find a number less than the pivot, or hit the
+            // left pointer.
             while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
                 rightPointer--;
             }
+
             swap(array, leftPointer, rightPointer);
         }
+
+        // This is different from what the video has, and fixes an issue where the last
+        // value could potentially be out of order.
+        // Thanks to viewer Wilson Barbosa for suggesting the fix!
         if (array[leftPointer] > array[highIndex]) {
             swap(array, leftPointer, highIndex);
         } else {
             leftPointer = highIndex;
         }
+
         return leftPointer;
     }
 
@@ -63,5 +84,12 @@ public class QuickSortYoutubeVersion {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
+    }
+
+    private static void printArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+        System.out.println();
     }
 }
